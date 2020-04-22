a316d0d245d011d5560bdd61aaf206a638c5f352
Sagar Adhikari <sagradhkr48@gmail.com>
2020-04-22 21:26:15 +0545

    getters and setter in C#

---
+++ Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace begineer_guide
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+
+        public Movie(string aTitle,string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            rating = aRating;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace begineer_guide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Getters and Setters.....
+
+            Movie avengers = new Movie("Once upon a time in holyhood", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            Console.ReadLine();
+
+
+            //Book book1 = new Book();
+            //book1.title = "sagar";
+            //book1.author = "adhikari";
+            //book1.pages = 400;
+            //Console.WriteLine(book1.pages);
+
+
+            //string secret = "sagar";
+            //string guess = "";
+            //int guessCount = 0;
+            //int guessLimit = 3;
+            //bool outOfGuesses = false;
+
+            //while (guess != secret && !outOfGuesses)
+            //{ 
+            //    if (guessCount < guessLimit)
+            //    {
+            //        Console.Write("Enter the guess:");
+            //        guess = Console.ReadLine();
+            //        guessCount++;
+            //    }
+            //    else
+            //    {
+            //        outOfGuesses = true;
+            //    }
+             
+            //}
+            //if (outOfGuesses)
+            //{
+            //    Console.WriteLine("you loose!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you win");
+            //}
+        
+           
+        }
+    }
+}
+
